8f90031011081843f84c41cc14703853587d7102
xiayan <xy@qq.com>
2024-06-03 17:23:34 +0800

    修复参数丢失的问题

--- mapper/BaseMapper.go
+++ mapper/BaseMapper.go
@@ -180,6 +180,7 @@ func Update(qw QueryWrapper) int64 {
 
 	if len(qw.updates) == 1 {
 		baseSQL = fmt.Sprintf("%s set %s = ? ", baseSQL, qw.updates[0].columns)
+		values = append(values, qw.updates[0].values)
 	} else {
 		for i := 0; i < len(qw.updates); i++ {
 			if !qw.updates[i].condition {
